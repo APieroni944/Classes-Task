332175df1575622ac2aa41c3ce1f006e4486d9bc
APieroni944 <st10479944@myemeris.edu.za>
2026-03-04 12:02:47 +0200

    Added files

---
+++ src/main/java/com/mycompany/person/Person.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.person;
+import javax.swing.*;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class Person {
+    String Name;
+    int Age;
+    Person() {
+        this.Name = Input("Input Name: ");
+        this.Age = Integer.parseInt(Input("Input Age: "));
+    }
+    public String Input(String Prompt) {
+        return JOptionPane.showInputDialog(Prompt);
+    }
+    public static void main(String[] args) {
+        Student s1 = new Student();
+    }
+}
+class Student extends Person {
+    int SNumber;
+    Course Course;
+    int Year;
+    Student() {
+        SNumber = Integer.parseInt(Input("Input Student Number: "));
+        Course = new Course();
+        Year = Integer.parseInt(Input("Input Year: "));
+    }
+}
+class Course {
+    String Name;
+    String CourseID;
+    Course() {
+        Name = JOptionPane.showInputDialog("Input course name: ");
+        CourseID = JOptionPane.showInputDialog("Input course ID: ");
+    }
+}
